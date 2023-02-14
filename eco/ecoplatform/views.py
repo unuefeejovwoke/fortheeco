51d4fcc90ec431a1ebbe54cec55d7af9e74b3063
unuefeejovwoke <vuwill7114@gmail.com>
2023-02-14 21:00:05 +0100

    Created urls and default view to check page

--- eco/ecoplatform/views.py
+++ eco/ecoplatform/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h2>Welcome To Eco</h2>")
