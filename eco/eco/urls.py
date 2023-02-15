7af017f5d9603dd612046e64962b4b9910d05033
unuefeejovwoke <vuwill7114@gmail.com>
2023-02-15 21:37:23 +0100

    edited the user app and urls

--- eco/eco/urls.py
+++ eco/eco/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecoplatform.urls',namespace='eco')),
-    path('', include('ecousers.urls',namespace='ecousers')),
+    path('accounts/', include('ecousers.urls',namespace='ecousers')),
 ]
--- eco/ecousers/urls.py
+++ eco/ecousers/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.index, name='ecousers'),
+    path('login/', views.login, name='ecousers'),
 ]
