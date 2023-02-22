8d0b1da5674883d1302cf3bc497ac8de327b2c46
Oghenetejiritop <tjsouthernking@gmail.com>
2023-02-22 10:35:52 +0100

    updated the views.py and index.html for trending filters

--- eco/ecoplatform/views.py
+++ eco/ecoplatform/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Problem, Category
-
+from .filters import ProblemFilter, ProjectFilter
 # Create your views here.
+
 def index(request):
     category_list = Category.objects.all()
-    projects = Project.objects.filter(isTrending=True)
-    problems = Problem.objects.filter(isTrending=True)
+    problem_filter = ProblemFilter(request.GET, queryset=Problem.objects.all())
+    project_filter = ProjectFilter(request.GET, queryset=Project.objects.all())
 
     context = {
         'category_list':category_list,
-        'projects': projects,
-        'problems': problems,
+        'problems': problem_filter,
+        'projects': project_filter,
     }
     return render(request,'ecoplatform/index.html',context)
-
-
-
