c248c202a46d01061217cdcfb8a8cf2b991011c9
Oghenetejiritop <tjsouthernking@gmail.com>
2023-03-03 06:21:01 +0100

    urls.py updated

--- eco/ecoplatform/urls.py
+++ eco/ecoplatform/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import index, problemListView, problemDetailView, projectListView, projectDetailView
+import views
 
 app_name = 'ecoplatform'
 
 
 urlpatterns = [
-    path('project/', projectListView, name='project_list'),
-    path('problem/', problemListView, name='problem_list'),
-    path('problem/<pk>/', problemDetailView, name='problem_detail'),
-    path('project/<pk>/', projectDetailView, name='project_detail'),
-    path('', index, name='home'),
+    path('project/', views.projectListView, name='project_list'),
+    path('problem/', views.problemListView, name='problem_list'),
+    path('problem/<pk>/', views.problemDetailView, name='problem_detail'),
+    path('project/<pk>/', views.projectDetailView, name='project_detail'),
+    path('', views.index, name='home'),
 ]
